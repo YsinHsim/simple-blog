a937ac31fa8d5fe9d67335304bd6523f154eacec
Yasin Hassim <45230727+YsinHsim@users.noreply.github.com>
2025-09-05 13:34:57 +0800

    Update Dashboard

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="p-8 m-4 bg-white rounded shadow w-full">
+
+        <div class="mb-4">
+            <H1Title>
+                Article List
+            </H1Title>
+        </div>
+
+        <div>
+            <div class="p-4 border-t border-b border-gray-200 ">
+                <NuxtLink>
+                    <p>My First Post</p>
+                </NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex h-screen w-full justify-center items-center">
+    <div class="p-4 bg-white rounded shadow">
+
+      <div class="mb-4">
+        <H1Title>
+          Welcome to My Simple Blog
+        </H1Title>
+      </div>
+
+      <p class="mb-2">
+        This is the homepage of the blog! There will be something inside, although i don't
+        really know what it is.
+      </p>
+
+      <NuxtLink to="/dashboard">
+        <PrimaryButton>
+          Enter Blog
+        </PrimaryButton>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
