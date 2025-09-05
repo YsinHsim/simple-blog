83f242f210bfe957388ce3b9b6e3cfeb2231e70d
Yasin Hassim <45230727+YsinHsim@users.noreply.github.com>
2025-09-05 13:34:37 +0800

    Setup Tailwind Config

---
+++ app.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <div class="flex min-h-screen w-full bg-gray-50">
+    <NuxtPage/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+useHead({
+  title: 'Yasin Blog'
+})
+</script>
